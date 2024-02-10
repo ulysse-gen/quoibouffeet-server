df15f5f87096924396c021332b130742cbbb29ec
ulysse-gen <ulysse1704@gmail.com>
2024-02-10 21:09:34 +0100

    Roved IDs from types API

--- src/assets/API/controllers/types.ts
+++ src/assets/API/controllers/types.ts
@@ -2,6 +2,7 @@ import express from "express";
 import db, { generateRandomIDForTable, query, queryTransaction } from "../../utilities/db";
 import _ from 'lodash';
 import Ingredient from "../../classes/Ingredients";
+import Type from "../../classes/Types";
 
 export async function createType(req: express.Request, res: express.Response) {
     let { name, description } = req.body;
@@ -69,7 +70,9 @@ export async function getType(req: express.Request, res: express.Response) {
         error: 'noType'
     });
 
-    return res.status(200).json(_.omit(TypeExists[0], ["id"]));
+    const type_ = TypeExists.map(typedata => new Type(typedata).clientVersion) as Array<Type>;
+
+    return res.status(200).json(type_);
 }
 
 export async function getTypeIngredients(req: express.Request, res: express.Response) {
@@ -88,7 +91,7 @@ export async function getTypeIngredients(req: express.Request, res: express.Resp
 
     const TypeIngredients = await db.query('SELECT ingredients_with_types.* FROM ingredients_with_types LEFT JOIN ingredients_types ON (ingredients_types.ingredientId = ingredients_with_types.id) LEFT JOIN types ON (types.id = ingredients_types.typeId) WHERE types.slug = ? OR types.name = ? OR types.id = ?;', [type, type, type]) as Array<QuoiBouffeEt.IngredientData>;
 
-    const Ingredients = TypeIngredients.map(ingredient => new Ingredient(ingredient));
+    const Ingredients = TypeIngredients.map(ingredient => new Ingredient(ingredient).clientVersion);
 
     return res.status(200).json(Ingredients);
 }
@@ -100,5 +103,7 @@ export async function getAllTypes(req: express.Request, res: express.Response) {
         error: 'noTypes'
     });
 
-    return res.status(200).json(TypeExists.map(type => _.omit(type, ["id"])));
+    const type = TypeExists.map(typedata => new Type(typedata).clientVersion) as Array<Type>;
+
+    return res.status(200).json(type);
 }
